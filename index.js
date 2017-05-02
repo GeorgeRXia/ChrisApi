8de88f8b9a7348a776d014829ca49910d6d973c9
tomatoandcheerio <Georxia@gmail.com>
2017-05-02 16:05:31 -0400

    picture format nice

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var pageContent; 
 var msg = "";
+var date;
+
+var containerDate = {
+
+1: "Jan",
+2: "Feb"
+
+
+};
 
 $(document).ready(function(){
 
@@ -22,22 +31,31 @@ $(document).ready(function(){
 			for(let i = 0; i < response.posts.length ; i++){
 
 
-				console.log(response.posts[i].categories[0]);
+				
 				console.log(response.posts[i].title);
 
+				date = response.posts[i].date.split("/");
+
+				var month = date[0];
+				var day = date[1];
+				console.log(date);
+
+				console.log(containerDate[month]);
 
 				msg += "<div class= 'pictures-content-container'>";
 				
-				msg += "<img class='pictures' src =" + response.posts[i].img + ">" + "</br>";
+				msg += "<img class='pictures' src =" + response.posts[i].img + ">";
 
-				msg += "<div class= 'content'";
-				msg +=  response.posts[i].date + "</div> " + "</br>" 
+				msg += "<div class= 'content'>";
+				msg +=  "<div class= 'lato'>" + containerDate[month] +" " + day + "</div>"; 
 
-				msg += response.posts[i].title + "</div>";
+				msg +=  "<div class= 'slabo'>"+ response.posts[i].title;
+
+				msg += "<div>" + "___" + "</div>";
 				
-				msg += "</br>" + response.posts[i].categories[0] + "</div>" ;
+				msg += "</div>" + "<div class= 'lato'>" + response.posts[i].categories[0]+ "</div>" + "</div>" ;
 
-				msg += </div>""
+				msg += "</div>"
 
 			}
 
